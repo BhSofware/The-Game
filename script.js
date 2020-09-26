f86aff50209256f8d0d0d8c59590ed3404517569
BhSofware <70692211+BhSofware@users.noreply.github.com>
2020-09-26 22:32:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+alert("Beste deelnemer");
+alert("Welkom bij de TopoQuiz. Bij deze quiz zijn er 2 categorieen ");
+alert("De 1e categorie: Hoofdsteden van West-Europese landen");
+alert("De 2e categorie: Hoofdsteden van Zuid-Europese landen");
+alert("ONTHOUD GOED DAT HOOFDSTEDEN ALTIJD MET EEN HOOFDLETTER BEGINNEN")
+alert("Laten we beginnen!");
+
+alert("CATEGORIE 1");
+
+var Engeland = prompt("Wat is de hoofdstad van Engeland?");
+
+if (Engeland == "Londen") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Duitsland = prompt("Wat is de hoofdstad van Duitsland?");
+if (Duitsland == "Berlijn") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Frankrijk = prompt("Wat is de hoofdstad van Frankrijk?");
+if (Frankrijk == "Parijs") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Italie = prompt("Wat is de hoofdstad van Italie?");
+if (Italie == "Rome") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Nederland = prompt("Wat is de hoofdstad van Nederland?");
+if (Nederland == "Amsterdam") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+alert("Tel je punten bij elkaar op bij de 1e categorie en dan gaan we beginnen met de 2e categorie");
+alert("CATEGORIE 2");
+
+var Spanje = prompt("Wat is de hoofdstad van Spanje?");
+if (Spanje == "Madrid") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}	
+
+var Portugal = prompt("wat is de hoofdstad van Portugal?");
+if (Portugal == "Lissabon") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt")
+}
+
+var Bosnie = prompt("Wat is de hoofdstad van Bosnie?");
+if (Bosnie == "Sarajevo"){
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Kroatie = prompt("Wat is de hoofdstad van Kroatie?");
+if (Kroatie == "Zagreb") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+var Montenegro = prompt("Wat is de hoofdstad van Montenegro?");
+if (Montenegro == "Podgorica") {
+	alert("Dit antwoord is goed, u heeft 1 punt gekregen");
+}else{
+	alert("Dit antwoord is fout, u krijgt geen punt");
+}
+
+alert("DIT WAS DE QUIZ, HET IS GEEN SPEL, MAAR WEL EDUCATIEF. IK HOOP DAT JE ER VAN HEBT GENOTEN, KIJK EFFE WAT JE SCORE IS EN ALS NIET GOED IS, BEGIN GERUST OPNIEUW")
+
+
+
+
+
+
+
+
